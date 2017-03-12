540ae40692f55c5dcd6ab7bebacbe976a5147932
Sunderous <vivi.orunitia@gmail.com>
2017-03-12 15:11:38 -0300

    Sensor model update

--- modules/brewing/server/models/sensor.server.model.js
+++ modules/brewing/server/models/sensor.server.model.js
@@ -4,33 +4,49 @@
  * Module dependencies
  */
 var mongoose = require('mongoose'),
-  Schema = mongoose.Schema;
+    Schema = mongoose.Schema;
 
 /**
  * Sensor Schema
  */
 var SensorSchema = new Schema({
-  sensorId: {
-    type: Number,
-    min: 1,
-    default: 1,
-    required: 'Sensor ID cannot be blank'
-  },
-  name: {
-    type: String,
-    default: 'Un-named sensor',
-    required: 'Sensor Name cannot be blank'
-  },
-  value: {
-    type: Number,
-    min: 0,
-    default: 0,
-    required: 'Sensor Value cannot be blank'
-  },
-  updated: {
-    type: Date,
-    default: Date.now()
-  }
+    sensorId: {
+        type: Number,
+        min: 1,
+        default: 1,
+        required: 'Sensor ID cannot be blank'
+    },
+    name: {
+        type: String,
+        default: 'Un-named sensor',
+        required: 'Sensor Name cannot be blank'
+    },
+    units: {
+        type: String,
+        default: "Undefined Units",
+        required: 'Sensor units cannot be undefined'
+    },
+    minValue: {
+        type: Number,
+        default: 0,
+        required: 'Sensor must have minimum value defined'
+    },
+    maxValue: {
+        type: Number,
+        default: 0,
+        required: 'Sensor must have maximum value defined'
+    },
+    value: {
+        type: Number,
+        min: minValue,
+        max: maxValue,
+        default: 0,
+        required: 'Sensor Value cannot be blank'
+    },
+    updated: {
+        type: Date,
+        default: Date.now()
+    }
 });
 
 mongoose.model('Sensor', SensorSchema);
