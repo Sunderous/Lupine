b89e2bb8805b676b81a65793e0fd7e7f48c122c7
Sunderous <vivi.orunitia@gmail.com>
2017-03-12 21:12:39 -0300

    chart maybe

--- modules/brewing/client/controllers/list-sensors.client.controller.js
+++ modules/brewing/client/controllers/list-sensors.client.controller.js
@@ -17,16 +17,16 @@
 
         init();
 
-        // var chart = new FusionCharts({
-        //     "type": "column2d",
-        //     "width": "500",
-        //     "height": "300",
-        //     "dataFormat": "json",
-        //     "dataSource": {
-        //         chart: {},
-        //         data: [{ value: 500 }, { value: 600 }, { value: 700 }]
-        //     }
-        // }).render("chartContainer");
+        var chart = new FusionCharts({
+            "type": "column2d",
+            "width": "500",
+            "height": "300",
+            "dataFormat": "json",
+            "dataSource": {
+                chart: {},
+                data: [{ value: 500 }, { value: 600 }, { value: 700 }]
+            }
+        }).render("chartContainer");
 
         function init() {
 
